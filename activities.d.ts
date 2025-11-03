db0e0f7f2608fef1c9189e9eeb05629c6db02046
foothillpulmo-png <foothillpulmo@gmail.com>
2025-11-02 23:31:09 -0800

    Create activities.d.ts

---
+++ activities.d.ts
@@ -0,0 +1,7 @@
+import { Hono } from 'hono';
+export declare const activityRoutes: Hono<{
+    Bindings: {
+        DB: D1Database;
+    };
+}, import("hono/types").BlankSchema, "/">;
+//# sourceMappingURL=activities.d.ts.map
