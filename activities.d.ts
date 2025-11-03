deac0604300a44c1baeb97a96a3304d83ea34da1
foothillpulmo-png <foothillpulmo@gmail.com>
2025-11-02 23:41:13 -0800

    Delete activities.d.ts

--- activities.d.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono';
-export declare const activityRoutes: Hono<{
-    Bindings: {
-        DB: D1Database;
-    };
-}, import("hono/types").BlankSchema, "/">;
-//# sourceMappingURL=activities.d.ts.map
